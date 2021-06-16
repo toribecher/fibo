0e0fa7377449696cbb32bbb38431cca5e3189f84
John Slota <john.slota@gmail.com>
2021-06-16 11:56:39 -0400

    serve fib test

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -2,15 +2,28 @@ package handlers
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestFibHandler(t *testing.T) {
 	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
 	// pass 'nil' as the third parameter.
-	_, err := http.NewRequest("GET", "/fibonacci", nil)
+	req, err := http.NewRequest("GET", "/fibonacci/12", nil)
 	if err != nil {
 		t.Fatal(err)
+	}
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(FibHandler)
+
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
 	}
 }
 
